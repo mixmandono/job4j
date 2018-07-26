4ea82849d1ff886fbec47ff1ec89efa64c9f9465
Pavel <pavelkolos777@gmail.com>
2018-07-26 13:22:02 +0300

    1.3. JavDoc. Code convetion.

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,18 @@
+package ru.job4j.calculate;
+
+/**
+ *Class Класс для вывода сообщения.
+ *@author kolos
+ *@since 26.07.2018
+ *@version 1
+ */ 
+
+public class Calculate {
+	/**
+	 * Main.
+	 * @param args - args
+	 */
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
