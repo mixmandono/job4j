f8b231d225348bd425cff8e81e17d1c838fd7e63
Pavel <pavelkolos777@gmail.com>
2018-08-01 13:00:06 +0300

    3.4. Расстояние между точками в системе координат

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -2,7 +2,7 @@ package ru.job4j.calculator;
 
 public class Calculator {
     private double result;
-    
+
     public void add(double first, double second) {
         this.result = first + second;
     }
